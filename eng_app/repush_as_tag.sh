4a6a48ed11d2aaceb67cb98b7bfabbd6aaa73ec0
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-04-04 16:33:43 +0900

    remove sudo

--- eng_app/repush_as_tag.sh
+++ eng_app/repush_as_tag.sh
@@ -6,15 +6,15 @@ if [ -z $tag ]; then
   tag=1
 fi
 
-#delete sudo docker image rm miyakz1192/eng_app:1 docker image
+#delete docker image rm miyakz1192/eng_app:1 docker image
 
-sudo docker image list | grep "miyakz1192/eng_app " | awk '{print $2}' | while read line
+docker image list | grep "miyakz1192/eng_app " | awk '{print $2}' | while read line
 do
-  sudo docker image rm miyakz1192/eng_app:${line}
+  docker image rm miyakz1192/eng_app:${line}
 done
 
-sudo docker build -t eng_app . --no-cache
-sudo docker tag `sudo docker image list | grep "eng_app " | awk '{print $3}'` miyakz1192/eng_app:${tag}
-sudo docker push miyakz1192/eng_app:${tag}
+docker build -t eng_app . --no-cache
+docker tag `docker image list | grep "eng_app " | awk '{print $3}'` miyakz1192/eng_app:${tag}
+docker push miyakz1192/eng_app:${tag}
 
 
