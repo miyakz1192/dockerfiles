e931ba73b61f523adf7ade047cb19caa95eb8047
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-04-25 18:08:56 +0900

    jenkins related

--- jenkins/init.sh
+++ jenkins/init.sh
@@ -1,6 +1,7 @@
 set -x
 
 pvcreate /dev/sda
+vgcreate docker /dev/sda
 lvcreate  --wipesignatures y -n thinpool docker -l 95%VG --verbose
 lvcreate --wipesignatures y -n thinpoolmeta docker -l 1%VG
 lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta  
